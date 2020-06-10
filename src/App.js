20773e8fdd0b5018e06b6f0692f986dab3642d74
Vinícius Benedito <vinicius.benedito98@gmail.com>
2020-06-10 21:30:40 +0000

    Versão 2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import api from './api';
 
 function App() {
-  return <h1>ReactJS, Template!</h1>
-}
 
+  const [vinhos, setVinhos] = useState([]);
+
+    useEffect(() => {
+        api.get('/vinho').then((response) => {
+            const itens = response.data;
+            setVinhos(itens);
+        })
+    }, [])
+
+
+  return (
+      <table>
+          {vinhos.map(item => (
+                <tr key={item.id}>
+                    <td>{item.id}</td>
+                    <td>{item.nome}</td>
+                    <td>{item.tipo}</td>
+                    <td>{item.classificacao}</td>
+                    <td>{item.safra}</td>
+                </tr>         
+          ))}
+      </table>
+  );
+
+}
 export default App;
